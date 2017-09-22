c3e79551b9c373d65f4ec13058cd5a84221626aa
NikuuSamma <nickgarcialara@gmail.com>
2017-09-22 00:56:57 -0500

    Finalized code for project 1 before feedback

---
+++ projects/project1/script.js
@@ -0,0 +1,30 @@
+function checkform(form) 
+{
+	// get all the inputs within the submitted form
+	var inputs = form.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) 
+	{
+		// only validate the inputs that have the required attribute
+        if(inputs[i].hasAttribute("required"))
+		{
+            if(inputs[i].value.length == 0)
+			{
+               document.getElementsByTagName('input')[i].style.backgroundColor="red";
+			}
+		}
+	}
+	return true;
+}
+
+var inputs = document.querySelectorAll('.input');
+for (var j = 0; j < inputs.length; j++) 
+{
+    inputs[j].addEventListener('click', reset);
+}
+	
+function reset()
+{	
+	document.getElementById(this.id).getElementsByTagName('input')[0].style.backgroundColor="white"; 
+}
+
+
